f9841f0062bbe35325dea3d45c2e759bc91c5f3d
kanish chauhan <kanishchauhan959@gmail.com>
2023-01-23 20:54:01 +0530

    basic template of the website made

--- app.js
+++ app.js
@@ -1,10 +1,41 @@
 const express=require('express')
 const app=express()
-const hbs=require('express-handlebars')
+const exphbs=require('express-handlebars')
+const path=require('path')
 const mysql=require('mysql')
+const router=require('./router/router')
+
 
 require('dotenv').config()
 const port=process.env.port||8000
+
+//parsing middleware
+//parese application/x-www-form-urlencoded
+app.use(express.urlencoded({extended:true}))
+
+// parse application/json
+app.use(express.json())
+
+// static css files
+app.use(express.static(path.join(__dirname,'public')))
+
+app.engine('hbs',exphbs.engine({extname:'.hbs'}))
+app.set('view engine', 'hbs')
+
+const connection=mysql.connection({
+    host:process.env.db_host,
+    database:process.env.db_name,
+    user:process.env.db_username,
+    password:process.env.db_password
+
+})
+
+
+
+
+app.use(router)
+
+
 
 app.listen(port, ()=>{
     console.log(`listening on port ${port}`)
