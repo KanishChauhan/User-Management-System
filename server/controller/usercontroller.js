8cbece054e8a2bcc77f900a910039a936ec7dbd0
kanish chauhan <kanishchauhan959@gmail.com>
2023-01-24 22:26:04 +0530

    we can create a new user in database and can also find it in the database

--- server/controller/usercontroller.js
+++ server/controller/usercontroller.js
@@ -17,31 +17,104 @@ password:process.env.db_password
 
 
 
-//VIEW USER
+// VIEW USER
 module.exports.view=(req,res)=>{
 
-    // res.render('home')
-    con.connect((err)=>{
-        if(err){
-            console.log('error connecting' + err)
-        }
        
-        con.query('SELECT email FROM USER WHERE id=1',(err,rows)=>{
+        con.query('SELECT * FROM USER',(err,rows)=>{
 
             if(!err){
                 let removedusers = req.query.removed
                 console.log('DATABASE CONNECTED')
                 res.render('home',{rows})
-
+                
             }
             else{
-                console.log('error occured')
+                console.log(err+'error occured')
             }
-            console.log(process.env.p)
-            // con.destroy()
         })
+      
+}
 
 
-    })
+
+module.exports.find=(req,res)=>{
+    // con.connect((err)=>{
+    //     if(err){
+    //         console.log(err)
+    //     }
+
+        let searchterm=req.body.search
+
+        // let t=Number.parseInt(searchterm)
+        // console.log(isNaN(searchterm))
+        if(searchterm.length==1 && !isNaN(searchterm)){
+           var qry=`SELECT * FROM USER WHERE id=${searchterm} `
+        //    console.log(qry)
+        }
+        if(searchterm=='active'){
+            qry=`SELECT * FROM USER WHERE status='${searchterm}'`
+        }
+        else if(searchterm.includes('@')){
+            
+            var qry=`SELECT * FROM USER WHERE email='${searchterm}' `
+            // console.log(qry)
+        }
+        else {
+
+            var qry=`SELECT * FROM USER WHERE first_name like '%${searchterm}%' OR last_name like '%${searchterm}%' OR email like '%${searchterm}%' OR phone like '%${searchterm}%'`
+        }
+        con.query(qry,(err,rows)=>{
+
+            if(!err){
+                res.render('home',{rows})
+                // console.log(rows.length)
+            }
+        })
+        
+        
+        // con.end()
+    // })
+}
+
+module.exports.add=(req,res)=>{
+    // con.connect((err)=>{
+    //     if(err){
+
+        //         console.log('error found' +err)
+        //     }
+        res.render('adduser')
+        // con.end()
+        // })
+    }
+
+module.exports.create=(req,res)=>{
+   
+
+        const {first_name,last_name,email,phone,comments}=req.body
+
+        
+        con.query('INSERT INTO user SET first_name = ?, last_name = ?, email = ?, phone = ?, comments = ?', [first_name, last_name, email, phone, comments],(err,rows)=>{
+            if(!err){
+                res.render('adduser',{alert:"USER ADDED SUCCESSFULLY"})
+            }
+            else{
+                console.log(err)
+            }
+         
+
+            
+        })
+   
+
+}
+
+
+
+module.exports.edit=(req,res)=>{
+    res.render('edituser')
+
     
 }
+
+//
--- server/router/router.js
+++ server/router/router.js
@@ -5,6 +5,11 @@ const user=require('../controller/usercontroller');
 ////////////////////////////////
 // console.log(user.view)
 router.get('/',user.view)
+router.post('/',user.find)
+router.get('/adduser',user.add)
+router.post('/adduser',user.create)
+router.get('/edituser/:id',user.edit)
+// router.post('/edituser/:id',user.edit)
 
 
 
