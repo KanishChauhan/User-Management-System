e9f6d00261f7f48dab2078cc573ce27d34f0de0d
kanish chauhan <kanishchauhan959@gmail.com>
2023-01-24 16:46:04 +0530

    .env untracked file

--- server/controller/usercontroller.js
+++ server/controller/usercontroller.js
@@ -1,22 +1,24 @@
 const mysql=require('mysql')
 
-// require('dotenv').config()
+require('dotenv').config()
 
 
 const con=mysql.createConnection({
 
 connectionLimit: 100,
-database:'user_management_system',
-host:'localhost',
-user:"root",
-password:'Kanish2225!'
+database:process.env.db_name,
+host:process.env.db_host,
+user:process.env.db_username,
+password:process.env.db_password
 });
+
+
 
 
 //VIEW USER
 module.exports.view=(req,res)=>{
 
-    res.render('home')
+    // res.render('home')
     con.connect((err)=>{
         if(err){
             console.log('error connecting' + err)
@@ -26,15 +28,17 @@ module.exports.view=(req,res)=>{
 
             if(!err){
                 let removedusers = req.query.removed
+                console.log('DATABASE CONNECTED')
                 res.render('home',{rows})
 
             }
             else{
                 console.log('error occured')
             }
-            console.log(rows)
-            con.destroy()
+            console.log(process.env.p)
+            // con.destroy()
         })
+
 
     })
     
